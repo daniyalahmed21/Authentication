a41e22946b701d95f6ac92c2f470e2b0795bfde7
Daniyal Ahmed <da9033431@gmail.com>
2025-03-02 22:02:19 +0500

    authentication

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import authConfig from "./auth.config"
+import NextAuth from "next-auth"
+ 
+
+export const { auth } = NextAuth(authConfig)
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  console.log(req.nextUrl.pathname);
+  console.log("Is Logged In :" + isLoggedIn);
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
